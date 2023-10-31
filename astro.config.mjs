b603bc5b8646f7de6b91323999c6a3fa0236b759
Fernando Corrales <killoconq@Fer.local>
2023-10-30 22:49:09 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
-});
+export default defineConfig({});
